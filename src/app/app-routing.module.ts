da895276f1d43dde21ef40c08849c7f4cee2edcb
Zsanett <zsanett1220@gmail.com>
2018-11-04 21:19:31 +0000

    Some structure change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { TutorialComponent } from './tutorial/tutorial.component';
 import { HomeComponent } from './home/home.component';
+import { LoopsComponent } from './loops/loops.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'tutorials', component: TutorialComponent },
+  { path: 'loops', component: LoopsComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home', pathMatch: 'full' }
 ];
