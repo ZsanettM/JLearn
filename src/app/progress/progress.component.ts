03221627c34979bff813b2206e09c630482b4a38
ZsanettM <1501846@abertay.ac.uk>
2019-02-23 14:38:38 +0000

    sample barchart added for leaderboard

--- src/app/progress/progress.component.ts
+++ src/app/progress/progress.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Network, Node, Edge, DataSet, IdType, Graph2d } from 'vis';
+import { Component, OnInit, ChangeDetectorRef, ViewChild, ElementRef } from '@angular/core';
+import {  DataSet,Graph2d } from 'vis';
+import { Chart } from 'chart.js';
 import 'vis/dist/vis-timeline-graph2d.min.css';
 import { ProgressTrackService } from '../shared/progress/progressTrack.service';
 import { Score } from '../shared/progress/score';
-import { count } from 'rxjs/operators';
 
 @Component({
   selector: 'app-progress',
@@ -11,35 +11,35 @@ import { count } from 'rxjs/operators';
   styleUrls: ['./progress.component.css']
 })
 
-/*
-interface ReturnedObject {
-  place: number
-  body: Score
-}*/
-
-export class ProgressComponent implements OnInit {
+export class ProgressComponent implements OnInit{
   private scoreObjs: Score[] = []
   private items: any[] =[]
   private currentTime: Date
   private tempTimeForEnd: Date
   private counter: number = 0
-  private uName: string; private uScore: number;
 
-  constructor(private service: ProgressTrackService) { }
+  private uName: string; private uScore: number; private uId:number; private defaultScreen: boolean = true;
+
+  @ViewChild('lbCanvas') canvas: ElementRef;
+  public context: CanvasRenderingContext2D;
+
+  constructor(private service: ProgressTrackService, private cd: ChangeDetectorRef) {
+   }
 
     public ngOnInit(): void {
       //get values from localStorage
       this.uName = localStorage.getItem("userName")
       this.uScore = Number(localStorage.getItem("userScore"))
+      this.uId = Number(localStorage.getItem("uid"))
 
       //get scores from DB
-      this.getProgressData(1);
+      this.getProgressData();
 
     }
 
-    getProgressData(userId: number){
+    getProgressData(){
 
-      this.service.getUserProgress(userId).subscribe(obj => {
+      this.service.getUserProgress(this.uId).subscribe(obj => {
         this.scoreObjs = obj
         obj.forEach(element => {
           this.counter += element.tutorial.points
@@ -50,14 +50,14 @@ export class ProgressComponent implements OnInit {
         });
         this.drawGraph()
       })
-
-      
-      //this.drawGraph()
-
-
     }
 
+    //---------Your Progress (vis.js)----------------//
     drawGraph(){
+      this.defaultScreen = true;
+      this.cd.detectChanges();
+
+      console.log(this.defaultScreen)
       var container = document.getElementById('mynetwork');   
       
       this.tempTimeForEnd = new Date(this.scoreObjs[this.scoreObjs.length-1].timestmp)
@@ -70,13 +70,51 @@ export class ProgressComponent implements OnInit {
         end: this.tempTimeForEnd,
         moveable: false,
         drawPoints: {
-          style: 'square' // square not working
+          style: 'square'
         },
         shaded: {
           orientation: 'bottom'
         }
       };
       var graph2d = new Graph2d(container, dataset, options);
+    }
+
+     //---------Leaderboard (chart.js)----------------//
+    drawCanvas(){
+      this.defaultScreen = false;
+      this.cd.detectChanges();
+
+      this.context = (<HTMLCanvasElement>this.canvas.nativeElement).getContext('2d');
+      var leaderBoard = new Chart(this.context, {
+        type: 'bar',
+        data: {
+          labels: ["Red", "Blue", "Yellow"],
+          datasets: [{
+            label: 'Top Scores',
+            data: [150, 135, 99],
+            backgoundColor:[
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)'
+            ],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              }
+            }]
+          }
+        }
+      })
     }
 
 
